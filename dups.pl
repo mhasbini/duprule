dcfa8ca0a3a6c2191b86850bf64dac5a91b79e2e
Mohammad <mohammad.hasbini@gmail.com>
2016-07-28 20:48:35 +0300

    Added dups.pl: a poc for the duplicate rules removing process

--- dups.pl
+++ dups.pl
@@ -1,9 +1,42 @@
 use strict;
 use warnings;
-use Data::Dumper;
+use Utils;
 use Rules;
+use File::Slurp qw/read_file/;
+use Data::Dumper;
+
+# $| = 1;
+my $start = time;
+
+print "- Loading rules ... ";
+my @rules = grep {substr($_, 0, 1) ne '#'} read_file($ARGV[0]);
+chomp @rules;
+print " loaded ". scalar @rules. " rule.\n";
+
 my $engine = Rules->new(verbose => 0);
-foreach my $rule (@ARGV) {
-	my $return = $engine->proccess($rule);
-	print Dumper $return;
+my $util = Utils->new();
+
+my %results;
+
+print "- Proccessing rules ... ";
+foreach my $rule (@rules) {
+	# print "\t-> $rule\n";
+	my $temp = $engine->proccess($rule);
+	$results{$rule} = $util->generate_id($temp) unless $temp eq "RULE_IS_NOT_SUPPORTED";
+}
+print "done in ", time - $start,"s.\n";
+
+$start = time;
+
+print "- Checking for duplicates ... ";
+my %reverse;
+while(	my(	$rule, $hash ) = each %results){
+    push @{$reverse{$hash}}, $rule;
+}
+print "done in ", time - $start,"s.\n";
+
+foreach my $hash (keys %reverse) {
+	if(scalar(@{$reverse{$hash}}) > 1) {
+		print "$hash -> ", join(', ', @{$reverse{$hash}}),"\n";
+	}
 }
